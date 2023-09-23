013bd229f02c8ab5666f2b609d4fd68d48126aaa
yewon4284 <142921713+yewon4284@users.noreply.github.com>
2023-09-23 11:06:21 +0900

    Initial commit

---
+++ makerapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MakerappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'makerapp'
---
+++ yewon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YewonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yewon'
